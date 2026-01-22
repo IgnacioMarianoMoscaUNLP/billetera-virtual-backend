9108fadc22d93398aea0079c48ef18606822782a
Ignacio Mariano <ignaciomarianomosca@gmail.com>
2026-01-22 13:01:22 -0300

    fisrt commit

---
+++ src/main/java/com/ignaciomariano/billetera/billetera_virtual_backend/BilleteraVirtualApplication.java
@@ -0,0 +1,13 @@
+package com.ignaciomariano.billetera.billetera_virtual_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BilleteraVirtualApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BilleteraVirtualApplication.class, args);
+	}
+
+}
