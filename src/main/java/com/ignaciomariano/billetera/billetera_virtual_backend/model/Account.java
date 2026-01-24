9bbe0e76a6442916132699e87748d4862eb0b742
Ignacio Mariano <ignaciomarianomosca@gmail.com>
2026-01-24 17:51:34 -0300

    mapping account class with jpa

--- src/main/java/com/ignaciomariano/billetera/billetera_virtual_backend/model/Account.java
+++ src/main/java/com/ignaciomariano/billetera/billetera_virtual_backend/model/Account.java
@@ -1,8 +1,26 @@
 package com.ignaciomariano.billetera.billetera_virtual_backend.model;
 
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity(name="account")
+@Table(name="accounts")
+@Getter @Setter
 public class Account {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.UUID)
+    private Long id;
+    @Column(unique = true)
     private String cvu;
+    @Column(nullable=false)
     private float balance;
+    @Column(nullable=false)
     private String currency;
+
+    //Relationship with user
+    @ManyToOne(fetch=FetchType.LAZY)
+    @JoinColumn(name="user_id",nullable=false)
+    private User accountHolder;
 }
